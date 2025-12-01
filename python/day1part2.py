01a1cdfca25883f89e2bc5dcc4bb17ed0125cd89
henley-regatta <42374576+henley-regatta@users.noreply.github.com>
2025-12-01 16:06:44 +0000

    Day 1 Part 2 - Done

---
+++ python/day1part2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# We have a DIAL with numbers 0-99
+# We have an INPUT which specifies a letter L/R for rotation
+# and a number giving the number of steps to move
+# We start from 50
+# PART 2 elaboration: count the number of times
+# the dial MOVES PAST zero
+
+inputfile="data/day1.txt"
+
+dialpos=50
+dialmax=99
+dialmin=0
+range=(dialmax-dialmin)+1
+
+with open(inputfile,'r') as fh:
+    scount=0
+    zerocount=0
+    print(f"{scount:<6} Starting at {dialpos}")
+    for move in fh:
+        scount+=1
+        dir=move[0]
+        steps=int(move.strip()[1:])
+        orgsteps = steps
+        if steps > range : #sneaky wotsits always pull this trick
+            zerocount += steps // range
+            steps = steps % range
+        if dir == "L" :
+            og=dialpos
+            dialpos-=steps
+            if dialpos<dialmin :
+                if og>0 :
+                    zerocount += 1
+                dialpos = range+dialpos
+        elif dir == "R" :
+            dialpos+=steps
+            if dialpos > range :
+                zerocount += 1
+                dialpos = dialpos % range
+            if dialpos==100 :
+                dialpos=0
+        if dialpos == 0 :
+            zerocount+=1
+        print(f"{scount:<10} {zerocount:<6} {dir} {orgsteps} to {dialpos}")
+print(f"Password: {zerocount}")
+#nb:  2755 < answer < 6734
